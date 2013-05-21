066ac90a7dc548cc300f8d63d719610a558500d7
Nico Sallembien <nsallembien@gmail.com>
2013-05-20 18:59:45 -0700

    Added Base localization

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Nico Sallembien on 5/20/13.
+//  Copyright (c) 2013 Transifex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
